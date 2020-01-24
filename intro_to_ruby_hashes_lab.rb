2b5791701cc9d88a192eb7a627eb229310f91023
Plimon Howard V <plimon.howard@gmail.com>
2020-01-24 21:10:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,8 +27,7 @@ end
 
 def read_from_hash(hash, key)
   rhsh = {hash => key}
-  return rhsh[hash]
-  # return the correct value using the hash and key parameters
+  return rhsh[:hash]
 end
 
 def update_counting_hash(hash, key)
