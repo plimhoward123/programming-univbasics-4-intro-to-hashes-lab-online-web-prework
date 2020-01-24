1b1e866899c8b97aad15455df5d0246405cfe905
Plimon Howard V <plimon.howard@gmail.com>
2020-01-24 21:09:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,6 +21,8 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
+  ahsh = {key => value}
+  return ahsh
   # return a hash that includes the key and value parameters passed into this method
 end
 
